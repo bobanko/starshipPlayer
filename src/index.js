2038fb1e5bb30aa048ef693dade4d58b98afa84c
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-08-05 23:46:55 +0300

    refac: ws client logic separated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-/**
- * Created by mishkovladimir on 05-Aug-17.
- */
+import {WsClient} from './ws-client';
+import {FrameDecoder} from './frame-decoder';
+import {Player} from './player';
+
+const frameDecoder = new FrameDecoder(frameDecoder_onPictureDecoded);
+const player = new Player(document.querySelector('canvas'), 240, 144);
+
+const wsClient1 = new WsClient('ws://localhost:9090/front', wsClientOnFrameGot);
+
+
+function wsClientOnFrameGot(...args) {
+    frameDecoder.decode(...args);
+}
+
+function frameDecoder_onPictureDecoded(frame) {
+    player.addFrame(frame);
+}
+
--- src/ws-client.js
+++ src/ws-client.js
@@ -1,3 +1,49 @@
-/**
- * Created by mishkovladimir on 05-Aug-17.
- */
+export class WsClient {
+
+    constructor(url, onFrameGot) {
+        this.socket = new WebSocket(url);
+        this.socket.binaryType = 'arraybuffer';
+
+        //subscriptions
+        this.socket.onopen = (event) => this.onOpen(event);
+        this.socket.onclose = (event) => this.onClose(event);
+        this.socket.onmessage = (event) => this.onMessage(event);
+        this.socket.onerror = (event) => this.onError(event);
+
+        //todo: make rx subs
+        this.onFrameGot = (frame) => onFrameGot(frame);
+    }
+
+
+    //todo: hide all these methods to private
+    onClose(event) {
+        if (event.wasClean) {
+            console.log('Соединение закрыто чисто');
+        } else {
+            console.log('Обрыв соединения'); // например, "убит" процесс сервера
+        }
+        console.log('Код: ' + event.code + ' причина: ' + event.reason);
+    }
+
+
+    onMessage(event) {
+        if (typeof event.data === "string") {
+            console.log("Получены данные " + event.data);
+            return;
+        }
+        //binary
+        let encodedFrame = new Uint8Array(event.data);
+
+        this.onFrameGot(encodedFrame);
+    }
+
+
+    onOpen() {
+        console.log("Соединение установлено.");
+    }
+
+    onError(error) {
+        console.log("Ошибка " + error.message);
+    }
+
+}
