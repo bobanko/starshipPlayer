ea6ff9f69151037369d1ddcb67b6d62a437104f8
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-08-05 23:46:38 +0300

    refac: frameDecoder-wrapper created

--- src/frame-decoder.js
+++ src/frame-decoder.js
@@ -1,3 +1,26 @@
-/**
- * Created by mishkovladimir on 05-Aug-17.
- */
+//import * as Decoder from 'broadwayjs';
+import * as Decoder from '../lib/Decoder';
+
+
+//todo: rxify all this shit - as subj
+
+//.next(put frame here)
+
+//.subscribe(get decoded frame here)
+
+
+export class FrameDecoder {
+
+
+    constructor(onPictureDecoded) {
+        this.decoder = new Decoder();
+
+        //todo: make rx
+        this.decoder.onPictureDecoded = onPictureDecoded;
+    }
+
+    decode(frame) {
+        this.decoder.decode(frame);
+    }
+
+}
