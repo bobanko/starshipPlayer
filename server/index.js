5cfb264ff7c3a2e6d5859c89e9031a97ffba459e
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-08-06 23:07:52 +0300

    cleanup

--- server/index.js
+++ server/index.js
@@ -3,5 +3,4 @@ const WsServer = require('./ws-server');
 
 httpServer({port: 9080});
 
-let wsServer1 = new WsServer({ port: 9091}); //server: httpServer, path: '/front',
-//let wsServer2 = new WsServer({server: httpServer, path: '/back', port: 9091});
+let wsServer = new WsServer({port: 9091});
