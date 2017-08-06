e0486646a4e87b33ab9c8fab812fbdb7934dfd10
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-08-06 23:06:23 +0300

    player rx refac in progress

--- src/player.js
+++ src/player.js
@@ -1,32 +1,93 @@
 //import { Observable } from 'rxjs';
 import * as Rx from 'rxjs';
 
+const SEC = 1000;
+
 export class Player {
 
     constructor(canvas) {
 
         this.frameList = [];
 
+        this.frameListSubject = new Rx.Subject();
+
+        // this.frameListSubject
+        // //.throttleTime(100)
+        //     .zip(Rx.Observable.timer(0, 1000), x => x)
+        //     .map(frame => {return frame;})
+        //     .subscribe(frame => this.decode(frame));
+
         this.canvas = canvas;
         this.canvasCtx = this.canvas.getContext("2d");
         this.canvasBuffer = this.canvasCtx.createImageData(this.canvas.width, this.canvas.height);
 
         //start playing
-        this.shiftFrame();
+        //this.shiftFrame();
+
+        let fpsCounter = document.querySelector('.fps');
+
+        //fps counter
+        let lastSampleTime = 0;
+        this.frameCount = 0;
+        setInterval(() => {
+            let now = performance.now();
+            if (this.frameCount > 0) {
+                let currentFps = (this.frameCount / (now - lastSampleTime) * 1000).toFixed(2);
+                fpsCounter.textContent = currentFps;
+                this.frameCount = 0;
+            }
+            lastSampleTime = now;
+        }, SEC);
+
+
+        this.fpsInterval = SEC / 60;
+        this.lastDrawTime = performance.now();
+        //this.animate();
+
+        setInterval(() => this.shiftFrame(), SEC / 10);
+
+
+        window.shiftFrame = () => this.shiftFrame();
     }
 
     addFrame(frame) {
         this.frameList.push(frame);
+
+        this.frameListSubject.next(frame);
+
     }
+
 
     shiftFrame() {
         const frame = this.frameList.shift();
 
+        //todo: decode frames zaranee
         if (frame)
             this.decode(frame);
 
-        requestAnimationFrame(() => this.shiftFrame());
+        //requestAnimationFrame(() => this.shiftFrame());
         //setTimeout(() => this.shiftFrame(),1000/24);
+    }
+
+    animate(now) {
+        // request another frame
+        requestAnimationFrame((time) => this.animate(time));
+
+        // calc elapsed time since last loop
+        let elapsed = now - this.lastDrawTime;
+
+        // if enough time has elapsed, draw the next frame
+        if (elapsed > this.fpsInterval) {
+            // Get ready for next frame by setting lastDrawTime=now, but...
+            // Also, adjust for fpsInterval not being multiple of 16.67
+            this.lastDrawTime = now - (elapsed % this.fpsInterval);
+
+            this.frameCount++;
+
+            //drawNextFrame(now, canvas, ctx, currentFps);
+            this.shiftFrame();
+
+        }
     }
 
 
@@ -34,7 +95,7 @@ export class Player {
 
         let fps = 0; //0 is maxfps speed, 1 is 1
 
-        Rx.Observable.interval(1).throttle(() => Rx.Observable.timer(1000 / fps)).subscribe(x => console.log(x));
+        Rx.Observable.interval(1).throttle(() => Rx.Observable.timer(SEC / fps)).subscribe(x => console.log(x));
     }
 
 
