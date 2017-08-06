9218da42e6c589e0c785f9d68d5818acd4cef259
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-08-06 23:10:17 +0300

    video file lib RX refac

--- server/video-file-lib.js
+++ server/video-file-lib.js
@@ -3,61 +3,63 @@ const fs = require('fs');
 const ffprobe = require('ffprobe');
 const ffprobeStatic = require('ffprobe-static');
 
+const Rx = require('Rxjs');
+
+
+function mapFileInfo(fileInfo, filePath) {
+    return {
+        filePath: filePath,
+        fileSize: fs.statSync(filePath)['size'],
+        width: fileInfo.width,
+        height: fileInfo.height,
+        frameCount: /*fileInfo.nb_frames ||*/ 200, //todo: missing
+        duration: /*fileInfo.duration ||*/ 5, //todo: missing
+        frameRate: fileInfo.r_frame_rate,
+    }
+}
+
+
+function getFileInfoAsync(filePath) {
+    return new Promise(function (resolve, reject) {
+        ffprobe(filePath, {path: ffprobeStatic.path}, function (err, info) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(mapFileInfo(info.streams[0], filePath));
+        });
+    });
+}
+
 
 class VideoFileLib {
 
-    constructor() {
-        this._onFileReadyCallbacks = [];
+    constructor({path, extensionMask}) {
         this.files = [];
+
+        this.loadFiles(path, extensionMask);
+    }
+
+    getFileStream(fileNameMask = /./) {
+        return Rx.Observable.from(this.files.filter(fileName => fileNameMask.test(fileName)));
     }
 
 
-    getFiles(path, fileMask) {
+    loadFiles(path, extensionMask = /./) {
+
         fs.readdir(path, (err, items) => {
 
-            let promiseArr = items.filter(file => fileMask.test(file))
-                .map(file => path + file)
-                .map(filePath => this.getFileInfoAsync(filePath));
+            let promiseArr = items
+                .filter(fileName => extensionMask.test(fileName))
+                .sort((fileName1, fileName2) => fileName1 - fileName2)
+                .map(fileName => path + fileName)
+                .map(filePath => getFileInfoAsync(filePath));
 
-            Promise.all(promiseArr).then(files => files.forEach(info => this.fileIsReady(info)));
 
+            Promise.all(promiseArr)
+                .then(files => files.forEach(fileInfo => this.files.push(fileInfo)))
+                .then(() => console.log('all video files loaded'));
         })
     }
-
-
-    fileIsReady(fileInfo) {
-        this.files.push(fileInfo);
-        this._onFileReadyCallbacks.forEach(cb => cb(fileInfo));
-    }
-
-    onFileReady(callback) {
-        this._onFileReadyCallbacks.push(callback);
-    }
-
-    static mapFileInfo(fileInfo, filePath) {
-        return {
-            filePath: filePath,
-            width: fileInfo.width,
-            height: fileInfo.height,
-            frameCount: /*fileInfo.nb_frames ||*/ 200, //todo: missing
-            duration: /*fileInfo.duration ||*/ 5, //todo: missing
-            frameRate: fileInfo.r_frame_rate,
-        }
-    }
-
-    getFileInfoAsync(filePath) {
-        return new Promise(function (resolve, reject) {
-            ffprobe(filePath, {path: ffprobeStatic.path}, function (err, info) {
-                //console.log(filePath);
-                if (err) {
-                    return reject(err);
-                }
-
-                return resolve(VideoFileLib.mapFileInfo(info.streams[0], filePath));
-            });
-        });
-    }
-
 }
 
 
