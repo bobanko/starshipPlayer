dadddf4691b78c32547bfce349385b45c0424c2b
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-08-10 22:49:40 +0300

    streamerClient rxified

--- server/streamer-client.js
+++ server/streamer-client.js
@@ -4,27 +4,34 @@ const getStaticFeedPipe = require('./get-static-feed');
 
 module.exports = class StreamerClient {
 
-    constructor(wsSend) {
-        this.wsSend = wsSend;
+    constructor() {
+        this.onFrameReceived = new Rx.Subject();
     }
 
 
     startSubStream(fileInfo) {
-        return new Promise((resolve, reject) => {
+        return Rx.Observable.create((observer) => {
 
-            getStaticFeedPipe(fileInfo).subscribe(data => this.wsSend(data), reject, resolve);
+            getStaticFeedPipe(fileInfo).subscribe(
+                data => this.onFrameReceived.next(data),
+                () => observer.error(),
+                () => observer.complete());
 
+            return function () {
+                // Any cleanup logic might go here
+            }
         });
     }
 
-    startStreamSequence(stream) {
-
-        stream.concatMap(fileInfo => Rx.Observable.fromPromise(this.startSubStream(fileInfo)))
+    startStreamSequence(fileInfoObservable) {
+        //when substream completes - start new one
+        fileInfoObservable.concatMap(fileInfo => this.startSubStream(fileInfo))
             .subscribe(() => console.log('⏹ stream ended'));//just to start
     }
 
 
     dispose() {
+        delete this;//?
         //todo: dispose all shit
     }
 
