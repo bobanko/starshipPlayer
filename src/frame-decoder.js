7300e4898c8c5a9f481b0a37bddac7d642d87a47
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-10-17 01:23:58 +0300

    FIX: MAJOR bug related to fact that Uint8Array is object, and 3rd party decoder changes returned value over and over again

--- src/frame-decoder.js
+++ src/frame-decoder.js
@@ -7,7 +7,7 @@ export class FrameDecoder {
         let onFrameDecodedSubject = new Rx.Subject();
 
         this.decoder = new Decoder();
-        this.decoder.onPictureDecoded = (...args) => onFrameDecodedSubject.next(...args);
+        this.decoder.onPictureDecoded = (buffer, ...args) => onFrameDecodedSubject.next(new Uint8Array(buffer), ...args);
 
         this.onFrameDecoded = onFrameDecodedSubject.asObservable();
         this.decode = (frame) => this.decoder.decode(frame);
