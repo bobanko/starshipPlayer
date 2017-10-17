2d9cf49abea181fab07f63c1549ba59ba591c4d7
Mishko Vladimir <mishko.vladimir@gmail.com>
2017-10-17 03:55:13 +0300

    video sizes moved to config

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 const config = {
     wsPort: 8091,
     httpPort: 8080,
-    //videosDir: './videos/',
-    videosDir: './videos_test/',
-    //videosDir: './vids_test2/',
+    videosDir: './videos/',
+    //videosDir: './videos_test2/',
+    // videosDir: './vids_test2/',
+    videoSize: {width: 240, height: 144},
+
+    // videosDir: './vids_test2/',
+    // videoSize: {width: 352, height: 288},
 };
 
 config.port = config.wsPort;
